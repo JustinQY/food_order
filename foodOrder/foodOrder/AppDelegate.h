c53295df20da31a4679acfe9baa93450782e3906
qiaoyibo <justinqyb@gmail.com>
2024-05-07 21:38:57 +0800

    init  project

---
+++ foodOrder/foodOrder/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  foodOrder
+//
+//  Created by Apollo on 2024/5/7.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ foodOrder/foodOrder/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  foodOrder
+//
+//  Created by Apollo on 2024/5/7.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor blackColor];
+}
+
+
+@end
